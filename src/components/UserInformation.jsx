a23bc1cdeb40dfeeae037866b9bc87cc1085a0fa
Jouderian Nobre <jouderianjr@gmail.com>
2017-11-09 23:37:42 -0300

    Improve UserInformation component
    - Adds link to open the github user profile

--- src/components/UserInformation.jsx
+++ src/components/UserInformation.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-const UserInformation = ({ user }) => (
-  <div>
-    <img src={user.avatar_url} width={100} />
-    <h3>{user.login}</h3>
-    <p>{user.bio}</p>
-  </div>
-);
+const UserInformation = ({ user }) => {
+  const { avatar_url, login, html_url, bio } = user;
+  return (
+    <div>
+      <img src={avatar_url} width={100} />
+      <a href={html_url} target="_blank">
+        <h3>@{login}</h3>
+      </a>
+      <p>{bio}</p>
+    </div>
+  );
+};
 
 export default UserInformation;
